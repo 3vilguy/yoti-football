7d1241320b812dec872a94993f9d7e99801e365b
3vilguy <2122619+3vilguy@users.noreply.github.com>
2020-05-11 02:29:10 +0100

    Add User model with name, email and password. Update users endpoint to create new user in DB. Fix some deprication warning.

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = new express.Router();
+const User = require('../models/user')
 
-router.post('/api/users', (req, res) => {
-    res.send({ user: 'User 123' })
+router.post('/api/users', async (req, res) => {
+    const user = new User(req.body);
+
+    try {
+        await user.save();
+        res.status(201).send({ user });
+    } catch (e) {
+        res.status(400).send(e);
+    }
 });
 
 module.exports = router;
