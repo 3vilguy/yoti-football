888e5db345f34ddd4cf1705db6818e21f2a105f7
3vilguy <2122619+3vilguy@users.noreply.github.com>
2020-05-11 03:01:46 +0100

    Hash user password before storing in DB.

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -26,5 +27,18 @@ const User = mongoose.model('User', {
         trim: true
     }
 });
+
+// Hash the plain text before saving
+userSchema.pre('save', async function (next) {
+    const user = this;
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 10);
+    }
+
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
