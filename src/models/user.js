f6662cf54ec899e54d070e1b7fc8adbe40658482
3vilguy <2122619+3vilguy@users.noreply.github.com>
2020-05-14 01:38:00 +0100

    Generate JWT for users when creating a new one. Add new route for user login. It will look up user by email and check if password hash matches.

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -25,8 +26,38 @@ const userSchema = new mongoose.Schema({
         required: true,
         minlength: 7,
         trim: true
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this;
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
+    user.tokens = user.tokens.concat({ token });
+    await user.save();
+    return token;
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new Error('Unable to login.');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        throw new Error('Unable to login.');
+    }
+
+    return user;
+}
 
 // Hash the plain text before saving
 userSchema.pre('save', async function (next) {
